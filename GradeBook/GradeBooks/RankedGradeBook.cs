186eaec9585ea5af43bfa15a43cd57f623f1e200
DWolanski <wolanskidawid@gmail.com>
2020-08-23 22:54:07 +0200

    Override of GetLetterGrade method in RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,47 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            var twentyPercentOfStudents = Students.Count * 20 / 100;
+            var rankedStudents = new Dictionary<char, List<Student>> 
+            {
+                ['A'] = new List<Student>(),
+                ['B'] = new List<Student>(),
+                ['C'] = new List<Student>(),
+                ['D'] = new List<Student>(),
+                ['E'] = new List<Student>(),
+            };
+
+            var rankToStudentsMapEnumerator = rankedStudents.GetEnumerator();
+
+            foreach(var student in Students.OrderByDescending(x => x.AverageGrade))
+            {
+                var currentRank = rankToStudentsMapEnumerator.Current;
+                if (currentRank.Value.Count >= twentyPercentOfStudents)
+                {
+                    rankToStudentsMapEnumerator.MoveNext();
+                }
+
+                currentRank.Value.Add(student);
+            }
+
+            foreach(var rank in rankedStudents)
+            {
+                if(averageGrade >= rank.Value.Min(x => x.AverageGrade))
+                {
+                    return rank.Key;
+                }
+            }
+
+            return 'F';
         }
     }
 }
